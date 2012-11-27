9ed8850b068f568ee9b53c1b2309159a37343e32
mavimaster <fakemail@github>
2012-11-27 16:30:31 +0100

    1st commit

---
+++ egittest1/src/main/java/ch/zhaw/chronontest/ChrononTest1Main.java
@@ -0,0 +1,18 @@
+package ch.zhaw.chronontest;
+
+public class ChrononTest1Main {
+
+	private static final int LOOPS = 10000000;
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		double a = 0;
+		for (int i = 0; i < LOOPS; i++) {
+			a = Math.pow(i, 2);
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end - start);
+		System.out.println("LAST a: " + a);
+	}
+
+}
